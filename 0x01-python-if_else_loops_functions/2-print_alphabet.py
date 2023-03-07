d9c589701f4ec2a893fc63eb951828189f83b9d3
mohammedmuk <mohammedmuk20@gmail.com>
2023-03-07 15:48:57 -0800

    new edit

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 values = range(97, 123)
 for value in values:
-    if value == 122:
-        print(f"{chr(value)}")
-        break
-
     print("{:s}".format(chr(value)), end = "")
-
