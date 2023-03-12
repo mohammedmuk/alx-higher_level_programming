68615efed5e824ce5b281c783831b549c6075fcc
mohammedmuk <mohammedmuk20@gmail.com>
2023-03-12 14:58:32 -0700

    new edit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@
 def print_matrix_integer(matrix=[[]]):
     for fr_mt in matrix:
         for sc_mt in fr_mt:
-            if fr_mt.index(sc_mt) == 2:
+            if fr_mt.index(sc_mt) == len(fr_mt) - 1:
                 print("{:d}".format(sc_mt), end='')
             else:
                 print("{:d} ".format(sc_mt), end='')
