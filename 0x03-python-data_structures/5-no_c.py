bcb5bf347ff88ef76278cf09ce1cb8a3797d2013
mohammedmuk <mohammedmuk20@gmail.com>
2023-03-13 14:40:10 -0700

    new edit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for st in my_string:
-        if st == "c":
-            table = my_string.maketrans("c", " ")
-            my_string.translate(table)
-            return my_string
-        elif st == "C":
-            table = my_string.maketrans("C", " ")
-            my_string.translate(table)
-            return my_string
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            del i
+
+    return my_string
