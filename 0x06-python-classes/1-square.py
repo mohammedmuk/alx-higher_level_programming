f1d4acc228365a456e317397ea0ee895c56e3165
mohammedmuk <mohammedmuk20@gmail.com>
2023-03-28 15:52:42 -0700

    new file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """Not Empty class"""
+    def __init__(self, size):
+        """ init function
+            Args:
+            pram1: size
+        """
+        self.__size = size
