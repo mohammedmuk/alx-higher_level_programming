3635720896ab679f93ba0cfd0fa2b16357c85eba
mohammedmuk <mohammedmuk20@gmail.com>
2023-04-03 13:38:59 -0700

    new edit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ Setter function make apdate to width to new value"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -38,7 +38,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ Setter function make apdate to height to new value"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
