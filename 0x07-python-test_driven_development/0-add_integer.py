2fd3c7170c5385bd37591d2e2463706d78e1144d
sadeemmuk <sadeemmuk2012@gmail.com>
2023-04-05 14:21:05 +0200

    new edit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+
 
 """
    This is module to add-integer function
@@ -20,3 +20,16 @@ def add_integer(a, b=98):
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/0-add_integer.txt")
+
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
