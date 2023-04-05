783693d20934b3ccae130c79be171348e2e69afb
mohammedmuk <mohammedmuk20@gmail.com>
2023-04-05 06:09:34 -0700

    new edit

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,7 +4,8 @@
 def matrix_divided(matrix, div):
     """
     matrix_divided function :
-    function take dimitional list and divid each element of this list by div arrgument
+    function take dimitional list and divid
+    each element of this list by div arrgument
 
     Return: new list (list_s)
     """
@@ -30,6 +31,7 @@ def matrix_divided(matrix, div):
 
         list_t.append(list_s)
     return list_t
+
 
 if __name__ == "__main__":
     import doctest
