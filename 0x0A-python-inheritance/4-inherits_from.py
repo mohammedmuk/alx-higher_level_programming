c2f9b60ceb55a1d67a7ffa99b5cbd5cbcf9f333e
sadeemmuk <sadeemmuk2012@gmail.com>
2023-04-12 23:54:52 +0200

    new edit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,5 @@
 def inherits_from(obj, a_class):
-    if isinstance(obj, a_class):
+    if type(obj) is not a_class:
         return True
     else:
         return False
-    
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
