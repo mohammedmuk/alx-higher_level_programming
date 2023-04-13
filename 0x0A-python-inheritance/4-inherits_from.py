7d93870fe62d9b8adc6e532c037619bdfff3f29d
sadeemmuk <sadeemmuk2012@gmail.com>
2023-04-14 00:35:13 +0200

    new files

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+
 
 """This is module for inherits_from function"""
 
@@ -11,3 +11,14 @@ def inherits_from(obj, a_class):
         return True
     else:
         return False
+
+# a = True
+
+# if inherits_from(a, int):
+#     print("{} inherited from class {}".format(a, int.__name__))
+# if inherits_from(a, bool):
+#     print("{} inherited from class {}".format(a, bool.__name__))
+# if inherits_from(a, object):
+#     print("{} inherited from class {}".format(a, object.__name__))
+
+print()
