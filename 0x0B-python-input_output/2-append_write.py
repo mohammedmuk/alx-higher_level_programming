93c31a89c92dee9df359a75a7feebac14bd31126
sadeemmuk <sadeemmuk2012@gmail.com>
2023-04-15 01:52:50 +0200

    new edit

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,6 +6,6 @@
 def append_write(filename="", text=""):
     """This is function for append new text"""
 
-    with open("file_append.txt", "a+", encoding="utf-8") as f:
+    with open(filename, "a+", encoding="utf-8") as f:
         write_data = f.write(text)
     return write_data
