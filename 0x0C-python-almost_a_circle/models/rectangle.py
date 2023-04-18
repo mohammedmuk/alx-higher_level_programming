14fb742fe31b38338b9a29cf599cfe363da270ca
sadeemmuk <sadeemmuk2012@gmail.com>
2023-04-18 03:19:31 +0200

    new edit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,6 +8,7 @@ class Rectangle(Base):
     """This is class for height and width and x and y attribute"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """This constractor consist main attribute"""
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -16,16 +17,20 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Width getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Width setter"""
         self.__width = value
 
     @property
     def height(self):
+        """Height getter"""
         return self.__height
 
     @height.setter
     def hight(self, value):
+        """Height setter"""
         self.__height = value
